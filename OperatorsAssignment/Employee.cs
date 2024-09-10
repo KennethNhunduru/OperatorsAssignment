4cf0c8440b2022228d3c40dba2df6cbb42b581fd
KennethNhunduru <133786329+KennethNhunduru@users.noreply.github.com>
2024-09-10 23:31:33 +0100

    Add project files.

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    //Creating an Employee class with Id, FirstName and LastName properties. 
+    class Employee
+    {
+        //Adding the properties.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //To make operations on user-defined data types is not as simple as the operations on built-in data types.
+        //To use operators with user-defined data types, they need to be overloaded according to the developer's needs.
+        //An operator can be overloaded by defining a FUNCTION to it.
+        //The function of the operator is declared by using the 'operator' keyword.
+
+        //The operator function is defined inside the class or structure whose objects/variables
+        //we require the overloaded operator to work with.
+        //The benefit of overloading an operator is to have the provision to make your own
+        //implementation of the operator. 
+
+        //Comparison operators (like <, >, ==) can be overloaded but MUST be overloaded in pairs. 
+
+        //Overloading the “==” operator In the Employee class, so it checks if two Employee objects
+        //are equal by comparing their Id property.
+
+        //First comparison
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool result = (employee1.Id == employee2.Id);
+            
+            return result;                            
+        }
+        //Second comparison 
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool result = (employee1.Id != employee2.Id);            
+            
+            return result;
+                      
+        }
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating the two employee objects for comparison.
+
+            Employee employee1 = new Employee() { Id = 10783, FirstName="Kunta", LastName="Kinte"};
+            Employee employee2 = new Employee() { Id = 12783, FirstName="Binta", LastName="Juwara"};
+
+            //Comparing the Employee Id's.
+            bool comparison = employee1 == employee2;
+            Console.WriteLine(comparison);
+            Console.ReadLine();          
+        }
+    }
+}
